9789bc4a619b1523f96e7e865ff86de049bcb148
AryaN <sunilkumaharana459@gmail.com>
2024-08-25 17:30:04 +0530

    Test_1

--- SunilKuMaharana1AT13/SunilKuMaharana_test1_at13.py
+++ SunilKuMaharana1AT13/SunilKuMaharana_test1_at13.py
@@ -364,4 +364,14 @@ select.select_by_visible_text("Option 1")"""
 
 "Q10"
 """10. Write xpath"""
-""""""
+"""from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver=webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.nseindia.com/")
+Action_Chain=ActionChains(driver)
+elemen_maket_data=driver.find_element(By.XPATH,"//li/a[text()='Market Data']")
+Action_Chain.move_to_element(elemen_maket_data).perform()
+driver.find_element(By.XPATH,"//li/a[text()='Pre-Open Market']").click()"""
